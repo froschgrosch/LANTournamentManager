654af55a4c5db0cef270f830e02b72676f954ed9
froschgrosch <65343661+froschgrosch@users.noreply.github.com>
2021-02-10 22:01:58 +0100

    Partial network protocol, small changes to client/server structure

--- Client.cs
+++ Client.cs
@@ -4,7 +4,10 @@
     class Client
     {
         Player p = new Player(0);
+        Tournament t = null;
+
         int status = 0; // see Tournament.cs
+        int id = 0;
 
         public Client()
         {
@@ -12,15 +15,25 @@
             p.setTag("UnnamedPlayer");
         }
 
+
+
         public bool isConnected() => status != 0;
         public int getStatus() => status;
         public void setStatus(int status) => this.status = status;
         public int getId() => p.getId();
         public void setId(int id) => p.setId(id);
-        public Player getPlayer() => p;
+        public Player getPlayer() {
+            Player pl = p;
+            if (true)
+            {
+
+            }
+            return pl;
+        }
         public void setPlayer(Player p) => this.p = p;
 
-        // NETWORKING
+        public Tournament getTournament() => t;
+
 
 
     }
--- Server.cs
+++ Server.cs
@@ -5,11 +5,51 @@
     class Server
     {
         Tournament t;
-        Client[] clients;
+
         int port;
         int num;
         string name;
         string game;
+
+        TcpClient[] tcpClients;
+        int numConnected = 0;
+        Client[] clients;
+
+        /* NETWORK PROTOCOL:
+         * formalities:
+         * 000 Welcome (connection successful) 000.OK
+         * 999 Bye (client or server) - 999.OK
+         * 998 Kick (server) - 998.OK
+         * 997 Server full (server) - 997.OK (bye from server);
+         * 
+         * player data format: 
+         * 00x.0 for set null (s>c) or non-existent player (c>s>c)
+         * 00x.1.k.d.w.l
+         * 00x.1.OK
+         * 00x.2.name
+         * 00x.2.OK
+         * 00x.3.tag
+         * 00x.3.OK
+         * 
+         * 001 (Server requests client player data)
+         * 001.OK (Client acknowledges and proceeds to send data)
+         * 
+         * 004.i (Server sends client ID)
+         * 004.OK (Client acknowledges and changes own ID)
+         * 
+         * 002.i (Client requests player data of player i)
+         * 002.i.OK (Server acknowledges and sends player data of player i)
+         * 
+         * 003 (Client requests to change own data)
+         * 003.OK (Server approves and registers changes)
+         * 003.DEN (Server denies changes)
+         * 
+         * 100.CHAT (Client sends chat)
+         * 100.OK (Server acknowledges and broadcasts to other clients)
+         * 
+         * 101.i.CHAT (Chat from client i)
+         *
+         */
 
         public Server(int num, int port, string name, string game)
         {
@@ -21,33 +61,71 @@
             clients = new Client[num];
             t = new Tournament(num, name, game);
 
-            ServerEventClass st = new ServerEventClass();
-            st.nc += new ServerEventClass.newClient(newClientEvent);
+            clientListener st = new clientListener();
+            st.nc += new clientListener.newClient(newClientEvent);
+
+            tcpClients = new TcpClient[num];
         }
 
         public Tournament getTournament() => t;
-
         public void setStatus(int i) => t.setStatus(i);
         public int getStatus() => t.getStatus();
 
-        private void newClientEvent(TcpClient c) { 
-        
+        protected virtual void newClientEvent(TcpClient c)
+        {
+            if (numConnected < clients.Length)
+            {
+
+            } 
+            else // server is full, send FULL and quit
+            {
+
+            }
         }
+
+        private void sendToClient(TcpClient c, string message)
+        {
+            NetworkStream stream = c.GetStream();
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(message);
+            stream.Write(msg, 0, msg.Length);
+        }
+
+
     }
 
-    // http://www.hinzberg.net/csharp/csharp/csharp/threadevents.html
-    // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=net-5.0
-    // https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient?view=net-5.0
+    //http://www.hinzberg.net/csharp/csharp/csharp/threadevents.html
+    //https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?view=net-5.0
+    //https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcpclient?view=net-5.0
 
-    class ServerEventClass
+    class clientListener
     {
         TcpListener listen;
         public delegate void newClient(TcpClient c);
         public event newClient nc;
-        public ServerEventClass()
+        public clientListener()
         {
             listen = new TcpListener(IPAddress.Parse("127.0.0.1"), 20000);
             listen.Start();
+            while (true) // wait for incoming 
+            {
+                nc(listen.AcceptTcpClient());
+            }
+        }
+
+
+    }
+
+    class messageListener
+    {
+        public delegate void newMessage(string s);
+        public event newMessage nm;
+
+        TcpClient client;
+        public messageListener(TcpClient client)
+        {
+            this.client = client;
+            
+            
         }
     }
 }
